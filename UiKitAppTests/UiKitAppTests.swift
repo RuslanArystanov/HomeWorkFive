298eaa23d05e4110ea66dcbad99dab30e46dfe53
ruslanarystanov <r.arystanov@arbuz.kz>
2024-09-24 20:05:54 +0500

    Initial Commit

---
+++ UiKitAppTests/UiKitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UiKitAppTests.swift
+//  UiKitAppTests
+//
+//  Created by Руслан Арыстанов on 24.09.2024.
+//
+
+import Testing
+@testable import UiKitApp
+
+struct UiKitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
