371cf2b71b641cc62974cf11baa6db02c849a0c7
ruslanarystanov <r.arystanov@arbuz.kz>
2024-11-05 19:04:16 +0500

    последняя версия 2

--- UiKitApp/ColorViewController.swift
+++ UiKitApp/ColorViewController.swift
@@ -22,9 +22,14 @@ class ColorViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var greenTextField: UITextField!
     @IBOutlet var blueTextFiel: UITextField!
     
-    private var redProgress: Float = 0.0
-    private var greenProgress: Float = 0.0
-    private var blueProgress: Float = 0.0
+    var redProgress: Float = 0.0
+    var greenProgress: Float = 0.0
+    var blueProgress: Float = 0.0
+    
+    var redSliderValue: Float = 0.0
+    var greenSliderValue: Float = 0.0
+    var blueSliderValue: Float = 0.0
+    
     
     var mainDelegate: MainDelegate!
     
@@ -36,42 +41,65 @@ class ColorViewController: UIViewController, UITextFieldDelegate {
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
         
-        redProgressLabel.text = String(redSlider.value)
-        greenProgressLabel.text = String(greenSlider.value)
-        blueProgressLabel.text = String(blueSlider.value)
+        redSlider.value = redSliderValue
+        greenSlider.value = greenSliderValue
+        blueSlider.value = blueSliderValue
         
-        redTextField.text = String(redSlider.value)
-        greenTextField.text = String(greenSlider.value)
-        blueTextFiel.text = String(blueSlider.value)
+        redProgressLabel.text = String(format: "%.2f", redSlider.value)
+        greenProgressLabel.text = String(format: "%.2f", greenSlider.value)
+        blueProgressLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        redTextField.text = String(format: "%.2f", redSlider.value)
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        blueTextFiel.text = String(format: "%.2f", blueSlider.value)
         
         redTextField.delegate = self
         greenTextField.delegate = self
         blueTextFiel.delegate = self
+        
+        setBackgroundColor(
+            redColor: CGFloat(redProgress),
+            greenColor: CGFloat(greenProgress),
+            blueColor: CGFloat(blueProgress)
+        )
     }
     
     @IBAction func colorAdjustment(_ sender: UISlider) {
         switch sender {
         case redSlider:
             redProgress = sender.value
+            redSliderValue = sender.value
             redProgressLabel.text = String(format: "%.2f", sender.value)
             redTextField.text = String(format: "%.2f", sender.value)
         case greenSlider:
             greenProgress = sender.value
+            greenSliderValue = sender.value
             greenProgressLabel.text = String(format: "%.2f", sender.value)
             greenTextField.text = String(format: "%.2f", sender.value)
         default:
             blueProgress = sender.value
+            blueSliderValue = sender.value
             blueProgressLabel.text = String(format: "%.2f", sender.value)
             blueTextFiel.text = String(format: "%.2f", sender.value)
         }
         
-        setBackgroundColor(redColor: CGFloat(redProgress),
-                           greenColor: CGFloat(greenProgress),
-                           blueColor: CGFloat(blueProgress))
+        setBackgroundColor(
+            redColor: CGFloat(redProgress),
+            greenColor: CGFloat(greenProgress),
+            blueColor: CGFloat(blueProgress)
+        )
         
-        mainDelegate.updateColor(red: CGFloat(redProgress),
-                             green: CGFloat(greenProgress),
-                             blue: CGFloat(blueProgress))
+        mainDelegate.updateColor(
+            red: CGFloat(redProgress),
+            green: CGFloat(greenProgress),
+            blue: CGFloat(blueProgress)
+        )
+        
+        mainDelegate.updateSliderValue(
+            redSliderValue: redSliderValue,
+            greeSliderValue: greenSliderValue,
+            blueSliderValue: blueSliderValue
+        )
 
     }
     
@@ -82,32 +110,52 @@ class ColorViewController: UIViewController, UITextFieldDelegate {
 
 extension ColorViewController {
     private func setBackgroundColor(redColor: CGFloat, greenColor: CGFloat, blueColor: CGFloat) {
-        colorLabel.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+        colorLabel.backgroundColor = UIColor(
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
+            alpha: 1
+        )
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case redTextField:
             redSlider.value = Float(redTextField.text ?? "0")!
+            redSliderValue = redSlider.value
             redProgressLabel.text = String(format: "%.2f", redSlider.value)
             redProgress = redSlider.value
         case greenTextField:
             greenSlider.value = Float(greenTextField.text ?? "0")!
+            greenSliderValue = greenSlider.value
             greenProgressLabel.text = String(format: "%.2f", greenSlider.value)
             greenProgress = greenSlider.value
         default:
             blueSlider.value = Float(blueTextFiel.text ?? "0")!
+            blueSliderValue = blueSlider.value
             blueProgressLabel.text = String(format: "%.2f", blueSlider.value)
             blueProgress = blueSlider.value
         }
         
-        mainDelegate.updateColor(red: CGFloat(redProgress),
-                             green: CGFloat(greenProgress),
-                             blue: CGFloat(blueProgress))
+        mainDelegate.updateColor(
+            red: CGFloat(redProgress),
+            green: CGFloat(greenProgress),
+            blue: CGFloat(blueProgress)
+        )
         
-        setBackgroundColor(redColor: CGFloat(redProgress),
-                           greenColor: CGFloat(greenProgress),
-                           blueColor: CGFloat(blueProgress))
+        setBackgroundColor(
+            redColor: CGFloat(redProgress),
+            greenColor: CGFloat(greenProgress),
+            blueColor: CGFloat(blueProgress)
+        )
+        
+        mainDelegate.updateSliderValue(
+            redSliderValue: redSliderValue,
+            greeSliderValue: greenSliderValue,
+            blueSliderValue: blueSliderValue
+        )
+        
+        textField.resignFirstResponder()
         
         return true
     }
--- UiKitApp/MainViewController.swift
+++ UiKitApp/MainViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 protocol MainDelegate {
     func updateColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+    func updateSliderValue(redSliderValue: Float, greeSliderValue: Float, blueSliderValue:Float)
 }
 
 class MainViewController: UIViewController {
     @IBOutlet var mainView: UIView!
+    private var redColor: Float = 0.0
+    private var greenColor: Float = 0.0
+    private var blueColor:Float = 0.0
+    
+    private var redSliderValue: Float = 0.0
+    private var greeSliderValue: Float = 0.0
+    private var blueSliderValue: Float = 0.0
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let colorVC = segue.destination as? ColorViewController else {
@@ -20,11 +28,29 @@ class MainViewController: UIViewController {
         }
         
         colorVC.mainDelegate = self
+        colorVC.redProgress = redColor
+        colorVC.greenProgress = greenColor
+        colorVC.blueProgress = blueColor
+        
+        colorVC.redSliderValue = redSliderValue
+        colorVC.greenSliderValue = greeSliderValue
+        colorVC.blueSliderValue = blueSliderValue
     }
+    
 }
 
 extension MainViewController: MainDelegate {
     func updateColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
         mainView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        redColor = Float(red)
+        greenColor = Float(green)
+        blueColor = Float(blue)
+    }
+    
+    func updateSliderValue(redSliderValue: Float, greeSliderValue: Float, blueSliderValue: Float) {
+        self.redSliderValue = redSliderValue
+        self.greeSliderValue = greeSliderValue
+        self.blueSliderValue = blueSliderValue
     }
 }
